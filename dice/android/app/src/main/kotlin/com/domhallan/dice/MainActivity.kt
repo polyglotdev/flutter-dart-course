7c86ab5b3b8eaa2aa063732731a413e09fd7b0e9
Dom Hallan <dom@domhallan.com>
2024-05-31 17:40:51 -0400

    Refactor project structure and update identifiers
    The project has been restructured with files renamed from 'demo' to 'dice'. This includes changes in the Android and iOS directories, as well as updates to the application's namespace and bundle identifier. The main.dart file was also deleted. Additionally, app icons have been updated across various resolutions.

---
+++ dice/android/app/src/main/kotlin/com/domhallan/dice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.domhallan.dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ dice/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.green],
+          ),
+        ),
+        child: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    ),
+  ));
+}
